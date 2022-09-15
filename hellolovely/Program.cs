e4123d6160e4db2012938a1f06a9ac31d3a4d4f6
Milena <emilieguryeva@gmail.com>
2022-09-15 17:10:23 +0300

    first commikjht

---
+++ hellolovely/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите имя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+
+    Console.WriteLine("Привет, крошка");
+}
+else 
+{
+    Console.Write ("hi, ");
+    Console.Write (username);
+}
